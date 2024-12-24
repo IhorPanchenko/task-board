692dfec74fa2657315465230ac92b2a0feb2885c
Ihor Panchenko <ipanchenko91@gmail.com>
2024-12-24 19:00:28 +0100

    Add redux to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { StyleSheetManager } from "styled-components";
 import { Suspense } from "react";
 import "./index.css";
 import App from "./App.jsx";
+import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <Suspense fallback={<div>Loading...</div>}>
-    <BrowserRouter>
-      <StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
-        <App />
-      </StyleSheetManager>
-    </BrowserRouter>
-  </Suspense>
+  <Provider store={store}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
+          <App />
+        </StyleSheetManager>
+      </BrowserRouter>
+    </Suspense>
+  </Provider>
 );
