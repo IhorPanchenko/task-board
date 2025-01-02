f9a7c109d0d13f3ad43697a8eb9315b24b473b19
Ihor Panchenko <ipanchenko91@gmail.com>
2025-01-02 18:54:55 +0100

    Refactor modal rendering into a helper function for cleaner JSX structur

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,159 +1,75 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   IoChevronDownOutline,
   IoChevronUpOutline,
   IoEllipsisVertical,
 } from "react-icons/io5";
+
 import logo from "../../assets/logo.svg";
+import boardsSlice from "../../redux/boardsSlice";
+
 import HeaderDropdown from "./HeaderDropdown";
 import AddEditBoardModal from "../modals/AddEditBoardModal";
 import AddEditTaskModal from "../modals/AddEditTaskModal";
 import ElipsisMenu from "../ElipsisMenu/ElipsisMenu";
 import DeleteBoardModal from "../modals/DeleteBoardModal";
-import boardsSlice from "../../redux/boardsSlice";
 
 const Header = ({ boardModalOpen, setBoardModalOpen }) => {
   const dispatch = useDispatch();
-
-  const [openDropdown, setOpenDropdown] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [openAddEditTask, setOpenAddEditTask] = useState(false);
-  const [isElipsisOpen, setIsElipsisOpen] = useState(false);
-  const [boardType, setBoardType] = useState("add");
+  const [modalState, setModalState] = useState({
+    openDropdown: false,
+    isDeleteModalOpen: false,
+    openAddEditTask: false,
+    isElipsisOpen: false,
+    boardType: "add",
+  });
 
   const boards = useSelector((state) => state.boards);
   const board = boards.find((board) => board.isActive);
 
-  const setOpenEditModal = () => {
+  const setOpenEditModal = useCallback(() => {
     setBoardModalOpen(true);
-    setIsElipsisOpen(false);
-  };
+    setModalState((prevState) => ({ ...prevState, isElipsisOpen: false }));
+  }, [setBoardModalOpen]);
 
-  const setOpenDeleteModal = () => {
-    setIsDeleteModalOpen(true);
-    setIsElipsisOpen(false);
-  };
+  const setOpenDeleteModal = useCallback(() => {
+    setModalState((prevState) => ({
+      ...prevState,
+      isDeleteModalOpen: true,
+      isElipsisOpen: false,
+    }));
+  }, []);
 
   const onDeleteBtnClick = () => {
     dispatch(boardsSlice.actions.deleteBoard());
     dispatch(boardsSlice.actions.setBoardActive({ index: 0 }));
-    setIsDeleteModalOpen(false);
+    setModalState((prevState) => ({ ...prevState, isDeleteModalOpen: false }));
   };
 
   const onDropdownClick = () => {
-    setOpenDropdown((state) => !state);
-    setIsElipsisOpen(false);
-    setBoardType("add");
+    setModalState((prevState) => ({
+      ...prevState,
+      openDropdown: !prevState.openDropdown,
+      isElipsisOpen: false,
+    }));
   };
 
-  return (
-    <>
-      {/* <nav className="md:w-[calc(100%-230px)] w-[calc(100%-60px)] gap-2 fixed flex items-center justify-between pl-2 pr-6 h-20 top-0 md:left-[230px] left-16 border-b border-slate-300 bg-white">
-        
-        <div className="flex items-center gap-3 cursor-pointer">
-          <IoPersonOutline color="#fb923c" size="1.75rem" />
-          <span className="text-orange-400 font-semibold md:text-lg text-sm whitespace-nowrap">
-            Board Name
-          </span>
-          <IoChevronDownOutline color="#fb923c" size="1rem" />
-        </div>
+  const toggleAddEditTask = () => {
+    setModalState((prevState) => ({
+      ...prevState,
+      openAddEditTask: !prevState.openAddEditTask,
+    }));
+  };
 
-        
-        <div className="flex items-center gap-2 md:w-[700px] w-32 bg-gray-100 rounded-lg px-3 py-3">
-          <IoSearchOutline color="#999999" />
-          <input
-            type="text"
-            placeholder="Search"
-            className="w-full bg-gray-100 outline-none text-sm"
-          />
-        </div>
-
-        
-        <div className="md:flex hidden items-center gap-4">
-          {[
-            { icon: IoShareSocialOutline, label: "Share" },
-            { icon: IoSettingsOutline, label: "Settings" },
-            { icon: IoNotificationsOutline, label: "Notifications" },
-          ].map(({ icon: Icon, label }) => (
-            <button
-              key={label}
-              className="grid place-items-center bg-gray-100 rounded-full p-2 cursor-pointer hover:bg-gray-200 transition-colors"
-              aria-label={label}
-            >
-              <Icon color="#444444" />
-            </button>
-          ))}
-        </div>
-      </nav> */}
-
-      <div className="p-4 fixed left-0 bg-white dark:bg-[#2b2c37] z-50 right-0">
-        <header className="flex justify-between dark:text-white items-center">
-          <div className="flex items-center space-x-2 md:space-x-4">
-            <img src={logo} alt="logo" className="h-6 w-6" />
-            <h3 className="hidden md:inline-block font-bold md:text-3xl">
-              Task Board
-            </h3>
-
-            <div className="flex items-center">
-              <h3 className="truncate max-w-48 md:text-2xl text-xl font-bold md:ml-20">
-                {board.name}
-              </h3>
-
-              <div
-                onClick={onDropdownClick}
-                className="w-3 ml-2 md:hidden cursor-pointer"
-              >
-                {openDropdown ? (
-                  <IoChevronUpOutline />
-                ) : (
-                  <IoChevronDownOutline />
-                )}
-              </div>
-            </div>
-          </div>
-
-          <div className="flex space-x-4 items-center md:space-x-6">
-            <button
-              onClick={() => {
-                setOpenAddEditTask((state) => !state);
-              }}
-              className="hidden md:block button"
-            >
-              + Add New Task
-            </button>
-
-            <button
-              onClick={() => {
-                setOpenAddEditTask((state) => !state);
-              }}
-              className="button py-1 px-3 md:hidden"
-            >
-              +
-            </button>
-
-            <IoEllipsisVertical
-              onClick={() => {
-                setBoardType("edit");
-                setOpenDropdown(false);
-                setIsElipsisOpen((state) => !state);
-              }}
-              className="w-6 h-6 text-gray-500 cursor-pointer"
-            />
-
-            {isElipsisOpen && (
-              <ElipsisMenu
-                type="Boards"
-                setOpenEditModal={setOpenEditModal}
-                setOpenDeleteModal={setOpenDeleteModal}
-              />
-            )}
-          </div>
-        </header>
-
+  const renderModals = () => {
+    const { openDropdown, isDeleteModalOpen, openAddEditTask, boardType } =
+      modalState;
+    return (
+      <>
         {openDropdown && (
           <HeaderDropdown
-            setOpenDropdown={setOpenDropdown}
+            setOpenDropdown={setModalState}
             setBoardModalOpen={setBoardModalOpen}
           />
         )}
@@ -169,7 +85,7 @@ const Header = ({ boardModalOpen, setBoardModalOpen }) => {
           <AddEditTaskModal
             type="add"
             device="mobile"
-            setIsAddTaskModalOpen={setOpenAddEditTask}
+            setIsAddTaskModalOpen={toggleAddEditTask}
           />
         )}
 
@@ -178,11 +94,87 @@ const Header = ({ boardModalOpen, setBoardModalOpen }) => {
             type="board"
             title={board.name}
             onDeleteBtnClick={onDeleteBtnClick}
-            setIsDeleteModalOpen={setIsDeleteModalOpen}
+            setIsDeleteModalOpen={(state) =>
+              setModalState((prevState) => ({
+                ...prevState,
+                isDeleteModalOpen: state,
+              }))
+            }
           />
         )}
-      </div>
-    </>
+      </>
+    );
+  };
+
+  return (
+    <div className="fixed left-0 right-0 p-4 z-50 bg-white dark:bg-[#2b2c37]">
+      <header className="flex items-center justify-between dark:text-white">
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <img src={logo} alt="logo" className="h-6 w-6" />
+          <h3 className="hidden md:inline-block font-bold md:text-3xl">
+            Task Board
+          </h3>
+
+          <div className="flex items-center">
+            <h3 className="md:ml-20 text-xl md:text-2xl font-bold max-w-48 truncate">
+              {board.name}
+            </h3>
+
+            <div
+              onClick={onDropdownClick}
+              className="w-3 ml-2 md:hidden cursor-pointer"
+            >
+              {modalState.openDropdown ? (
+                <IoChevronUpOutline />
+              ) : (
+                <IoChevronDownOutline />
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center space-x-4 md:space-x-6">
+          <button
+            onClick={toggleAddEditTask}
+            className="hidden md:block button"
+            aria-label="Add New Task"
+          >
+            + Add New Task
+          </button>
+
+          <button
+            onClick={toggleAddEditTask}
+            className="md:hidden py-1 px-3 button"
+            aria-label="Add New Task"
+          >
+            +
+          </button>
+
+          <IoEllipsisVertical
+            onClick={() => {
+              setModalState((prevState) => ({
+                ...prevState,
+                boardType: "edit",
+                isElipsisOpen: !prevState.isElipsisOpen,
+                openDropdown: false,
+              }));
+            }}
+            className="w-6 h-6 text-gray-500 cursor-pointer"
+          />
+
+          {modalState.isElipsisOpen && (
+            <ElipsisMenu
+              type="Boards"
+              setOpenEditModal={setOpenEditModal}
+              setOpenDeleteModal={setOpenDeleteModal}
+            />
+          )}
+        </div>
+      </header>
+
+      {/* Render all modals here */}
+      {renderModals()}
+    </div>
   );
 };
 
