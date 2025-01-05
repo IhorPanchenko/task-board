31464a1a8486f8080a68408afdd8772685b7cd81
Ihor Panchenko <ipanchenko91@gmail.com>
2025-01-05 14:04:55 +0100

    Refactor ElipsisMenu component: Improve readability, optimize re-renders, and enhance accessibility

--- src/components/ElipsisMenu/ElipsisMenu.jsx
+++ src/components/ElipsisMenu/ElipsisMenu.jsx
@@ -1,32 +1,38 @@
+import { useCallback } from "react";
+
 const ElipsisMenu = ({ type, setOpenEditModal, setOpenDeleteModal }) => {
+  const handleOpenEditModal = useCallback(() => {
+    setOpenEditModal();
+  }, [setOpenEditModal]);
+
+  const handleOpenDeleteModal = useCallback(() => {
+    setOpenDeleteModal();
+  }, [setOpenDeleteModal]);
+
   return (
     <div
       className={
-        type === "Boards" ? "absolute top-16 right-5" : "absolute top-6 right-4"
+        type === "Boards" ? "absolute right-5 top-16" : "absolute right-4 top-6"
       }
     >
       <div className="flex justify-end items-center">
         <div
-          className="w-42 text-sm z-50 font-medium shadow-md shadow-[#364e7e1a] 
-        bg-white dark:bg-[#20212c] space-y-4 py-5 px-4 rounded-lg  h-auto pr-12"
+          className="w-42 h-auto text-sm font-medium z-50 shadow-md shadow-[#364e7e1a] 
+        bg-white dark:bg-[#20212c] space-y-4 py-5 px-4 pr-12 rounded-lg"
         >
-          <span
-            onClick={() => {
-              setOpenEditModal();
-            }}
-            className="block cursor-pointer dark:text-gray-400 text-gray-700"
+          <button
+            onClick={handleOpenEditModal}
+            className="block cursor-pointer text-gray-700 dark:text-gray-400"
           >
             Edit {type}
-          </span>
+          </button>
 
-          <span
-            onClick={() => {
-              setOpenDeleteModal();
-            }}
+          <button
+            onClick={handleOpenDeleteModal}
             className="block cursor-pointer text-red-500"
           >
             Delete {type}
-          </span>
+          </button>
         </div>
       </div>
     </div>
