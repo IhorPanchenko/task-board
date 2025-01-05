57d7967273038325edd4f78cea7a20ef0c6b4efd
Ihor Panchenko <ipanchenko91@gmail.com>
2025-01-05 14:48:18 +0100

    Refactor Dropdown Component

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IoTimeOutline } from "react-icons/io5";
-import { formatDeadline } from "../../helpers/formatDeadline";
 import TaskModal from "../modals/TaskModal";
 
 const Task = ({ taskIndex, colIndex }) => {
--- src/helpers/getRandomImage.js
+++
@@ -1,7 +0,0 @@
-export const getRandomImage = (
-  width = 200,
-  height = 300,
-  seed = Math.random()
-) => {
-  return `https://picsum.photos/seed/${seed}/${width}/${height}`;
-};
