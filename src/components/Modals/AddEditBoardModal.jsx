2f3bcb3c111cd4a6692ff5e96a7c61139820706e
Ihor Panchenko <ipanchenko91@gmail.com>
2025-01-05 14:50:12 +0100

    Refactor AddEditBoardModal component

--- src/components/Modals/AddEditBoardModal.jsx
+++ src/components/Modals/AddEditBoardModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IoCloseOutline } from "react-icons/io5";
 import { v4 as uuidv4 } from "uuid";
@@ -7,92 +7,74 @@ import boardsSlice from "../../redux/boardsSlice";
 const AddEditBoardModal = ({ setIsBoardModalOpen, type }) => {
   const dispatch = useDispatch();
   const [name, setName] = useState("");
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
   const [isValid, setIsValid] = useState(true);
   const board = useSelector((state) => state.boards).find(
     (board) => board.isActive
   );
-
   const [newColumns, setNewColumns] = useState([
     { name: "Todo", task: [], id: uuidv4() },
     { name: "Doing", task: [], id: uuidv4() },
   ]);
 
-  if (type === "edit" && isFirstLoad) {
-    setNewColumns(
-      board.columns.map((col) => {
-        return { ...col, id: uuidv4() };
-      })
-    );
-    setName(board.name);
-    setIsFirstLoad(false);
-  }
+  useEffect(() => {
+    if (type === "edit" && board && newColumns.length === 2) {
+      setNewColumns(board.columns.map((col) => ({ ...col, id: uuidv4() })));
+      setName(board.name);
+    }
+  }, [board, type, newColumns.length]);
 
-  const handleChange = (id, newValue) => {
+  const handleColumnChange = (id, newValue) => {
     setNewColumns((prevState) => {
-      const newState = [...prevState];
-      const column = newState.find((col) => col.id === id);
-      column.name = newValue;
-      return newState;
+      const updatedColumns = prevState.map((col) =>
+        col.id === id ? { ...col, name: newValue } : col
+      );
+      return updatedColumns;
     });
   };
 
-  const handleDelete = (id) => {
-    setNewColumns((prevState) => prevState.filter((el) => el.id !== id));
+  const handleDeleteColumn = (id) => {
+    setNewColumns((prevState) =>
+      prevState.filter((column) => column.id !== id)
+    );
   };
 
   const validate = () => {
-    setIsValid(false);
-    if (!name.trim()) {
-      return false;
-    }
-
-    for (let i = 0; i < newColumns.length; i++) {
-      if (!newColumns[i].name.trim()) {
-        return false;
-      }
-    }
-
-    setIsValid(true);
+    if (!name.trim()) return false;
+    if (newColumns.some((column) => !column.name.trim())) return false;
     return true;
   };
 
-  const onSubmit = (type) => {
-    setIsBoardModalOpen(false);
-    if (type === "add") {
-      dispatch(boardsSlice.actions.addBoard({ name, newColumns }));
+  const handleSubmit = () => {
+    if (validate()) {
+      const action =
+        type === "add"
+          ? boardsSlice.actions.addBoard
+          : boardsSlice.actions.editBoard;
+      dispatch(action({ name, newColumns }));
+      setIsBoardModalOpen(false);
     } else {
-      dispatch(boardsSlice.actions.editBoard({ name, newColumns }));
+      setIsValid(false);
     }
   };
 
   return (
     <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-[#00000080] px-2 py-4 overflow-scroll scrollbar-hide"
       onClick={(e) => {
-        if (e.target !== e.currentTarget) {
-          return;
-        }
-        setIsBoardModalOpen(false);
+        if (e.target === e.currentTarget) setIsBoardModalOpen(false);
       }}
-      className="fixed right-0 left-0 top-0 bottom-0 px-2 py-4 scrollbar-hide overflow-scroll 
-      z-50 justify-center items-center flex bg-[#00000080]"
     >
-      <div
-        className="scrollbar-hide overflow-y-scroll max-h-[95vh] bg-white 
-      dark:bg-[#2b2c37] text-black dark:text-white font-bold shadow-md 
-      shadow-[#364e7e1a] max-w-md mx-auto w-full px-8 py-8 rounded-xl"
-      >
+      <div className="mx-auto w-full max-w-md max-h-[95vh] px-8 py-8 overflow-y-scroll bg-white dark:bg-[#2b2c37] text-black dark:text-white font-bold shadow-md shadow-[#364e7e1a] rounded-xl">
         <h3 className="text-lg">
           {type === "edit" ? "Edit" : "Add New"} Board
         </h3>
 
         <div className="mt-8 flex flex-col space-y-3">
-          <label className="text-sm dark:text-white text-gray-500">
+          <label className="text-sm text-gray-500 dark:text-white">
             Board Name
           </label>
           <input
-            className="bg-transparent px-4 py-2 rounded-md text-sm border 
-          border-gray-600 outline-none focus:outline-[#635fc7] outline-1 ring-0"
+            className="bg-transparent px-4 py-2 text-sm border border-gray-600 rounded-md outline-none focus:outline-[#635fc7]"
             placeholder="e.g Web Design"
             value={name}
             onChange={(e) => setName(e.target.value)}
@@ -101,7 +83,7 @@ const AddEditBoardModal = ({ setIsBoardModalOpen, type }) => {
         </div>
 
         <div className="mt-8 flex flex-col space-y-3">
-          <label className="text-sm dark:text-white text-gray-500">
+          <label className="text-sm text-gray-500 dark:text-white">
             Board Columns
           </label>
 
@@ -110,17 +92,12 @@ const AddEditBoardModal = ({ setIsBoardModalOpen, type }) => {
               <input
                 type="text"
                 value={column.name}
-                onChange={(e) => {
-                  handleChange(column.id, e.target.value);
-                }}
-                className="bg-transparent flex-grow px-4 py-2 rounded-md text-sm border
-              border-gray-600 outline-none focus:outline-[#735fc7]"
+                onChange={(e) => handleColumnChange(column.id, e.target.value)}
+                className="flex-grow bg-transparent px-4 py-2 text-sm border border-gray-600 rounded-md outline-none focus:outline-[#735fc7]"
               />
               <IoCloseOutline
-                onClick={() => {
-                  handleDelete(column.id);
-                }}
-                className="h-5 w-5 text-gray-600 cursor-pointer m-1"
+                onClick={() => handleDeleteColumn(column.id)}
+                className="h-5 w-5 m-1 text-gray-600 cursor-pointer"
               />
             </div>
           ))}
@@ -128,25 +105,20 @@ const AddEditBoardModal = ({ setIsBoardModalOpen, type }) => {
 
         <div>
           <button
-            onClick={() => {
-              setNewColumns((state) => [
-                ...state,
+            onClick={() =>
+              setNewColumns([
+                ...newColumns,
                 { name: "", task: [], id: uuidv4() },
-              ]);
-            }}
-            className="w-full items-center hover:opacity-75 dark:text-[#635fc7]
-          dark:bg-white text-white bg-[#635fc7] mt-4 py-2 rounded-full"
+              ])
+            }
+            className="w-full mt-4 py-2 rounded-full bg-[#635fc7] text-white hover:opacity-75 dark:bg-white dark:text-[#635fc7]"
           >
             + Add new column
           </button>
 
           <button
-            onClick={() => {
-              const isValid = validate();
-              if (isValid === true) onSubmit(type);
-            }}
-            className="w-full items-center hover:opacity-75 dark:text-white 
-          dark:bg-[#635fc7] mt-8 relative text-white bg-[#635fc7] py-2 rounded-full"
+            onClick={handleSubmit}
+            className="w-full mt-8 py-2 rounded-full bg-[#635fc7] text-white hover:opacity-75 dark:bg-[#635fc7] dark:text-white relative"
           >
             {type === "add" ? "Create New Board" : "Save Changes"}
           </button>
